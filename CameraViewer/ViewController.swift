873bf37c5eb938d6dca1dc029386926113339e0a
Leonardo Sanches <62853565+leonardolsanches@users.noreply.github.com>
2025-05-08 23:00:45 -0300

    mvp

--- CameraViewer/ViewController.swift
+++ CameraViewer/ViewController.swift
@@ -1,11 +1,36 @@
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var videoView: UIView!
+    private var player: AVPlayer?
+    private var playerLayer: AVPlayerLayer?
+
+    // Substitua pela URL real da sua câmera IP (MJPEG ou HLS)
+    private let cameraURL = URL(string: "http://your-camera-ip/stream.mjpeg")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupVideoView()
     }
-
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        sender.setTitle("Clicado!", for: .normal)
+    
+    private func setupVideoView() {
+        player = AVPlayer(url: cameraURL)
+        playerLayer = AVPlayerLayer(player: player)
+        if let playerLayer = playerLayer {
+            playerLayer.frame = videoView.bounds
+            playerLayer.videoGravity = .resizeAspect
+            videoView.layer.addSublayer(playerLayer)
+        }
+    }
+    
+    @IBAction func loadCameraButtonTapped(_ sender: UIButton) {
+        player?.play()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playerLayer?.frame = videoView.bounds
     }
 }
